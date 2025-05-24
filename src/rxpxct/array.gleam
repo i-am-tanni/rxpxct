c561d830f0f36e7d6aaa2d462a9be6ae576558be
tanni <tannisroot@proton.me>
2025-05-23 20:59:50 -0400

    Fixed redundant FFI

--- src/rxpxct/array.gleam
+++ src/rxpxct/array.gleam
@@ -5,7 +5,7 @@ pub type Array(a) {
   Array(a)
 }
 
-@external(erlang, "rxpxct_ffi", "from_list")
+@external(erlang, "array", "from_list")
 pub fn from_list(list: List(a)) -> Array(a)
 
 @external(erlang, "rxpxct_ffi", "get")
--- src/rxpxct/rxpxct_ffi.erl
+++ src/rxpxct/rxpxct_ffi.erl
@@ -1,12 +1,9 @@
 -module(rxpxct_ffi).
 
--export([get/2, from_list/1]).
+-export([get/2]).
 
 get(Array, Index) ->
     case catch array:get(Index, Array) of
         {'EXIT', _} -> {error, nil};
         E -> {ok, E}
     end.
-
-from_list(List) ->
-    array:from_list(List).
